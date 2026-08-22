e69e5670adcfbdcef4bf9c4c6f0bd5ed43dae27d
nnishant776 <nn54565852@gmail.com>
2026-08-23 00:52:23 +0530

    refactor: Update home-manager instalation and configuration

--- lib/mkHost.nix
+++ lib/mkHost.nix
@@ -18,40 +18,12 @@ let
     (hostDir + "/default.nix")
     ../modules/core
     ../modules/myconf
-    ({ config, ... }:
-    let
-      allUsers = [ config.myconf.host.adminUser ] ++ config.myconf.host.extraUsers;
-      hmUsers = builtins.filter (u: u.enableHomeManager) allUsers;
-    in {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        extraSpecialArgs = { inherit inputs; };
-        users = lib.listToAttrs (map (user: lib.nameValuePair user.name (
-          { pkgs, ... }: {
-            imports = [
-              ../modules/home
-              (user.extraHomeConfig or {})
-            ];
-            home = {
-              username = user.name;
-              homeDirectory = if isDarwin then "/Users/${user.name}" else "/home/${user.name}";
-            };
-            programs.git = {
-              settings.user = {
-                name = lib.mkDefault user.fullName;
-                email = lib.mkDefault (if user.email != "" then user.email else "${user.name}@localhost");
-              };
-            };
-          }
-        )) hmUsers);
-      };
-    })
+    { nixpkgs.hostPlatform = system; }
+    ../modules/home/home-manager.nix
   ];
 in
   if isLinux then
     inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
       modules = commonModules ++ [
         { system.stateVersion = "26.05"; }
       ]
@@ -65,7 +37,6 @@ in
     }
   else if isDarwin then
     inputs.nix-darwin.lib.darwinSystem {
-      inherit system;
       modules = commonModules ++ [
         ../modules/system/darwin
         inputs.home-manager.darwinModules.home-manager
