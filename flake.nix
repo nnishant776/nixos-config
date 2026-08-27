e8a6f01df8d96cd3b7532afb14062b2153060e6a
nnishant776 <nn54565852@gmail.com>
2026-08-28 02:33:55 +0530

    fix(modules/core): Enable caching for noctalia

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia";
+      url = "github:noctalia-dev/noctalia/cachix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- modules/core/default.nix
+++ modules/core/default.nix
@@ -4,7 +4,12 @@
     ./development
   ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [ "https://noctalia.cachix.org" ];
+    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
+
   nixpkgs.config = {
     allowUnfree = true;
     allowUnfreePredicate = (_: true);
